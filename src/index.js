6027313049261afdb6c3ef25fad09696c490c27d
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-09-13 22:54:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename="/car-rental">
+    <BrowserRouter basename="/car-rental">
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
