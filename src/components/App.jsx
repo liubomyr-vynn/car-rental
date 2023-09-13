1586c8028f8ffa948d8ef23017916e3af13713a9
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-09-13 22:41:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage';
+import CatalogPage from './pages/CatalogPage/CatalogPage';
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+import NotFound from './pages/NotFound/NotFound';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/catalog" element={<CatalogPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
+
+export default App;
