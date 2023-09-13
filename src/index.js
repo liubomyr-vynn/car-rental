3b6cbe38018dacecf146c8fe92b2f502a577a516
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-09-13 22:40:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router basename="/car-rental">
+      <App />
+    </Router>
   </React.StrictMode>
 );
