c381f0d4976766b911ade72e32bbe641c1356a0f
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-09-17 17:38:17 +0300

    update FavoritesPage

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,10 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Card from '../../components/Card/Card';
+import { CardContainer } from './FavoritesPage.styled';
 import Container from 'components/Container/Container';
 
 const FavoritesPage = () => {
+  const [favoriteCarIds, setFavoriteCarIds] = useState([]);
+  const [favoriteCars, setFavoriteCars] = useState([]);
+
+  useEffect(() => {
+    const favoritesData = JSON.parse(localStorage.getItem('favorites')) || [];
+    setFavoriteCarIds(favoritesData);
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get('https://648cbfdc8620b8bae7ed56b5.mockapi.io/advert')
+      .then(response => {
+        const carsData = response.data;
+
+        const favoriteCarsData = carsData.filter(car =>
+          favoriteCarIds.includes(car.id)
+        );
+        setFavoriteCars(favoriteCarsData);
+      })
+      .catch(error => {
+        console.error('Помилка при отриманні даних:', error);
+      });
+  }, [favoriteCarIds]);
+
+  const removeFavorite = carId => {
+    const updatedFavorites = favoriteCarIds.filter(id => id !== carId);
+    setFavoriteCarIds(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+  };
+
   return (
-    <>
-      <Container>FavoritesPage</Container>
-    </>
+    <Container>
+      <CardContainer>
+        {favoriteCars.map(car => (
+          <div key={car.id}>
+            <Card
+              car={car}
+              isFavoriteProp={true}
+              updateFavorites={() => removeFavorite(car.id)}
+            />
+          </div>
+        ))}
+      </CardContainer>
+    </Container>
   );
 };
 
---
+++ src/pages/FavoritesPage/FavoritesPage.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 29px;
+`;
+
+export const LoadMoreContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
